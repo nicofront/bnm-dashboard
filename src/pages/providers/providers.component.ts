a42bc4bf450f038d0f272afbf7b038d0af2c1c27
Nico Perez <nicolasperez@multiplica.com>
2023-03-06 15:14:33 -0300

    update - site colors

--- src/pages/providers/providers.component.ts
+++ src/pages/providers/providers.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
+// Estilo Banmedica
 // @Component({
 //   selector: 'app-providers',
 //   templateUrl: './providers.component.html',
 //   styleUrls: ['../home/home.component.scss', '../home/sitebanmedica.scss'],
 // })
 
+// Estilo Vidatres
 @Component({
   selector: 'app-providers',
   templateUrl: './providers.component.html',
