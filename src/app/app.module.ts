99672d31a0279862bd4e4f0ed9c63ce4abdd2787
Nico Perez <nicolasperez@multiplica.com>
2023-03-09 16:32:17 -0300

    chore - asegurados component and view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { NgChartsModule } from 'ng2-charts';
 import { TabsModule } from 'src/components/tabs/tabs.module';
 import { CfooterModule } from 'src/components/cfooter/cfooter.module';
+import { CardaseguradoModule } from 'src/components/card-asegurado/card-asegurado.module';
 import { HomeComponent } from 'src/pages/home/home.component';
 import { FooterComponent } from 'src/pages/pfooter/pfooter.component';
+import { AseguradosComponent } from 'src/pages/asegurados/asegurados.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule, TooltipModule, NgChartsModule, TabsModule, CfooterModule],
+  declarations: [AppComponent, HomeComponent, FooterComponent, AseguradosComponent],
+  imports: [BrowserModule, AppRoutingModule, TooltipModule, NgChartsModule, TabsModule, CfooterModule, CardaseguradoModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/components/card-asegurado/card-asegurado.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { Cardasegurado } from './card-asegurado.types';
+
+@Component({
+  selector: 'bm-card-asegurado',
+  styleUrls: ['card-asegurado.component.scss'],
+  templateUrl: 'card-asegurado.component.html',
+})
+export class CardaseguradoComponent {
+
+  constructor() {
+  }
+
+}
