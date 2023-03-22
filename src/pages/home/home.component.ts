417c2f31fd90f2520f0b4071184c8869bd80b9f3
Nico Perez <nicolasperez@multiplica.com>
2023-03-21 21:33:10 -0300

    chore - different charts

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -24,6 +24,18 @@ export class HomeComponent {
   public activedTab = 0;
   public isDocuemntsVisible = false;
 
+  // Colores Banmedica
+  public barColor1 = '#357B99';
+  public barColor2 = '#4C97B7';
+  public barColor3 = '#83C0DA';
+  public barColor4 = '#B8EAFF';
+
+  // Colores Vidatres
+  // public barColor1 = '#2d816b';
+  // public barColor2 = '#46a088';
+  // public barColor3 = '#82c6b2';
+  // public barColor4 = '#BAEDDD';
+
   public tabs = [
     {
       ico: '../assets/svg/hospital_coverage.svg',
@@ -47,36 +59,7 @@ export class HomeComponent {
     },
   ];
 
-  // chart Vidatres
-  // public barChartData: ChartConfiguration<'bar'>['data'] = {
-  //   labels: ['', '', '', ''],
-  //   datasets: [
-  //     {
-  //       data: [100, 80, 70, 50],
-  //       label: '',
-  //       backgroundColor: ['#2d816b', '#46a088', '#82c6b2', '#BAEDDD'],
-  //       barPercentage: 1.0,
-  //       categoryPercentage: 1.0,
-  //     },
-  //   ],
-  // };
-
-
-  // chart Banmedica
   public barChartData: ChartConfiguration<'bar'>['data'] = {
-    labels: ['', '', '', ''],
-    datasets: [
-      {
-        data: [100, 80, 70, 50],
-        label: '',
-        backgroundColor: ['#357B99', '#4C97B7', '#83C0DA', '#B8EAFF'],
-        barPercentage: 1.0,
-        categoryPercentage: 1.0,
-      },
-    ],
-  };
-
-  public barChartData2: ChartConfiguration<'bar'>['data'] = {
     labels: [''],
     datasets: [
       { 
@@ -85,6 +68,62 @@ export class HomeComponent {
         categoryPercentage: 1.0,
         label: '', 
         backgroundColor: '#9BA5B7' }
+    ],
+  };
+
+  // Tab0
+  public barChartData0: ChartConfiguration<'bar'>['data'] = {
+    labels: ['', '', '', ''],
+    datasets: [
+      {
+        data: [100, 80, 70, 50],
+        label: '',
+        backgroundColor: [this.barColor1, this.barColor2, this.barColor3, this.barColor4],
+        barPercentage: 1.0,
+        categoryPercentage: 1.0,
+      },
+    ],
+  };
+
+  // Tab1
+  public barChartData1: ChartConfiguration<'bar'>['data'] = {
+    labels: ['', '', ''],
+    datasets: [
+      {
+        data: [100, 80, 70],
+        label: '',
+        backgroundColor: [this.barColor1, this.barColor2, this.barColor3],
+        barPercentage: 1.0,
+        categoryPercentage: 1.0,
+      },
+    ],
+  };
+
+  // Tab2
+  public barChartData2: ChartConfiguration<'bar'>['data'] = {
+    labels: ['', ''],
+    datasets: [
+      {
+        data: [100, 80],
+        label: '',
+        backgroundColor: [this.barColor1, this.barColor2],
+        barPercentage: 1.0,
+        categoryPercentage: 1.0,
+      },
+    ],
+  };
+
+  // Tab3
+  public barChartData3: ChartConfiguration<'bar'>['data'] = {
+    labels: [''],
+    datasets: [
+      {
+        data: [100],
+        label: '',
+        backgroundColor: [this.barColor1],
+        barPercentage: 1.0,
+        categoryPercentage: 1.0,
+      },
     ],
   };
 
