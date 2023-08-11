4219b256bb4bee72debbbc8ed6d8545c740c15e4
Nicolas Perez <nicoperezre@gmail.com>
2023-08-10 22:45:41 -0400

    chore - accordion

--- src/components/modal-benefits/modal-benefits.component.ts
+++ src/components/modal-benefits/modal-benefits.component.ts
@@ -13,10 +13,15 @@ export class ModalbenefitsComponent {
   }
 
   id : number = 1;
+  status: boolean = false;
 
   benefitsGo(id: any) {
     // event.preventDefault();
     this.id = id;
+  }
+  accordiontg() {
+    console.log('yes!');
+    this.status = !this.status;   
   }
 
 }
