2e25762bc21c2d756373b294e6e584dd696cac9d
Nicolas Perez <nicoperezre@gmail.com>
2023-09-01 00:35:12 -0400

    password - steps

--- src/app/components/verification/verification.component.ts
+++ src/app/components/verification/verification.component.ts
@@ -9,6 +9,12 @@ export class VerificationComponent implements OnInit {
 
   constructor() { }
 
+  id : number = 1;
+
+  verificationGo(id: any) {
+    this.id = id;
+  }
+
   verificationKey(id: any, number: any) {
     var elements = document.getElementById(id)!.children
     var card:any = elements.item(number)!
